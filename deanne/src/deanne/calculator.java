71ad518fd3a2a9e156ae995162be907af85571db
Deanne Dcosta <dcostadeanne@gmail.com>
2024-04-27 10:39:45 +0530

    First commit

---
+++ deanne/src/deanne/calculator.java
@@ -0,0 +1,17 @@
+package deanne;
+
+import java.util.Scanner;
+
+public class calculator {
+
+	int num1=10;
+	int num2=5;
+	
+	void add() {
+		int sum=num1+num2;
+	}
+	
+	void sub() {
+		int sub=num1-num2;
+	}
+}
